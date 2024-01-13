b1f6aa64314cbdfda2dd4d3e75a98493fa5fd8c4
Abdusamaddev <abdusamaddev571@gmail.com>
2024-01-13 06:10:43 -0500

    About to notification

--- ai/tests.py
+++ ai/tests.py
@@ -6,24 +6,41 @@ import numpy as np
 import faiss
 import time
 import threading
-import random
 
 
 class CameraProcessor(threading.Thread):
-    def __init__(self, face_trainer, camera_index, threshold):
+    def __init__(self, face_trainer, camera_index, threshold, disappearance_timeout=5):
         super(CameraProcessor, self).__init__()
         self.face_trainer = face_trainer
         self.camera_index = camera_index
         self.threshold = threshold
+        self.disappearance_timeout = disappearance_timeout
+        self.last_detection_time = {}
 
     def run(self):
-        camera = cv2.VideoCapture(self.camera_index)
         while True:
-            ret, frame = camera.read()
-            self.face_trainer.process_frame(frame, self.threshold)
+            camera = cv2.VideoCapture(self.camera_index)
+            if not camera.isOpened():
+                print(f"Error: Could not open camera {self.camera_index}")
+                time.sleep(2)
+                continue
+            else:
+                print("Connected")
+            while True:
+                ret, frame = camera.read()
+                if not ret:
+                    print(
+                        f"Error: Could not read frame from camera {self.camera_index}"
+                    )
+                    break
+                self.face_trainer.process_frame(
+                    frame, self.threshold, self.camera_index, self.last_detection_time
+                )
+            camera.release()
+            time.sleep(1)
 
 
-class FaceTrainer:
+class Surveillance:
     def __init__(self, root_dir):
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -69,13 +86,21 @@ class FaceTrainer:
         else:
             return None
 
-    def process_frame(self, frame, threshold=0.6, save_path="screenshots"):
+    def process_frame(
+        self,
+        frame,
+        threshold=0.6,
+        camera_index=0,
+        last_detection_time=None,
+        save_path="screenshots",
+    ):
         if frame is None:
-            print("Error: Unable to read frame from the camera.")
+            print(f"Error: Unable to read frame from the camera {camera_index}.")
             return
 
         query_image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         query_faces = self.face_model.get(query_image)
+
 
         if not query_faces:
             return
@@ -83,11 +108,27 @@ class FaceTrainer:
         for face in query_faces:
             query_embedding = face.embedding
             result = self.search_similar_face(query_embedding, threshold)
-
             if result:
-                print(f"Similar face in the folder: {result}")
-                # Save the screenshot
-                self.save_screenshot(frame, result, save_path)
+                print("Last detection time: ", last_detection_time)
+                self.check_disappeared_faces(last_detection_time, face_id=result)
+                current_time = time.time()
+                if (
+                    result not in last_detection_time
+                    or (current_time - last_detection_time[result]) > threshold
+                ):
+                    rect_color = (0, 255, 0)  # Green color
+                    rect_thickness = 2
+                    bbox = face.bbox.astype(int)
+                    cv2.rectangle(
+                        frame,
+                        (bbox[0], bbox[1]),
+                        (bbox[2], bbox[3]),
+                        rect_color,
+                        rect_thickness,
+                    )
+                    print(f"Similar face in the folder: {result}")
+                    last_detection_time[result] = current_time
+                    self.save_screenshot(frame, result, save_path)
 
     def save_screenshot(self, frame, face_name, save_path):
         if not os.path.exists(save_path):
@@ -100,18 +141,22 @@ class FaceTrainer:
         cv2.imwrite(screenshot_path, frame)
         print(f"Screenshot saved: {screenshot_path}")
 
-    def process_camera_frames(
-        self,
-        camera_index="rtsp://admin:p@rolo12345@192.168.254.201:554/h264/ch41/main/av_stream",
-        threshold=600,
-    ):
-        camera = cv2.VideoCapture(camera_index)
+    def check_disappeared_faces(self, last_detection_time, face_id):
+        current_time = time.time()
+        last_seen = last_detection_time.get(face_id)
+        if last_seen is not None:
+            if (current_time - last_seen) > 5:
+                del last_detection_time[face_id]
+        # disappeared_faces = [
+        #     face
+        #     for face, last_time in last_detection_time.items()
+        #     if (current_time - last_time) > 5
+        # ]
+        # for face in disappeared_faces:
+        #     print(f"Face {face} has disappeared.")
+        #     del last_detection_time[face]
 
-        while True:
-            ret, frame = camera.read()
-            self.process_frame(frame, threshold)
-
-    def process_camera_frames_parallel(self, camera_urls, threshold=550):
+    def process_camera_frames_parallel(self, camera_urls, threshold=500):
         threads = []
         for camera_url in camera_urls:
             thread = CameraProcessor(self, camera_url, threshold)
@@ -120,19 +165,15 @@ class FaceTrainer:
         for thread in threads:
             thread.start()
 
-        # for thread in threads:
-        #     thread.join()
 
-
-# Example usage:
 root_directory = "/home/ubuntu/project/BazaarSurveillance/media/criminals/"
-face_trainer = FaceTrainer(root_directory)
+face_trainer = Surveillance(root_directory)
 
 # Specify multiple camera URLs
 camera_urls = [
+    f"rtsp://admin:p@rolo12345@192.168.254.201:554/h264/ch48/main/av_stream",
     f"rtsp://admin:p@rolo12345@192.168.254.201:554/h264/ch41/main/av_stream"
-    # for i in range(10, 150)
+    # for i in  [5,41,44,48,29,30,31,34,35,37]
 ]
 
-# Process frames from multiple cameras in parallel
 face_trainer.process_camera_frames_parallel(camera_urls)
