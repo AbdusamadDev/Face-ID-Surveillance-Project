781d06b275c665c8b996ac068e38c54dcaf1592e
Abdusamaddev <abdusamaddev571@gmail.com>
2024-01-20 06:51:31 -0500

    Backup

--- ai/tests.py
+++ ai/tests.py
@@ -31,6 +31,8 @@ class Surveillance:
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
         try:
+            self.exit_event = threading.Event()
+            self.lock = threading.Lock()
             self.face_model = insightface.app.FaceAnalysis()
             self.root_dir = root_dir
             ctx_id = 0
@@ -95,39 +97,41 @@ class Surveillance:
             query_embedding = face.embedding
             result = self.search_similar_face(query_embedding, 520)
             if result:
-                self.check_disappeared_faces(last_detection_time, face_id=result)
+                with self.lock:
+                    self.check_disappeared_faces(last_detection_time, face_id=result)
                 current_time = time.time()
-                if result not in last_detection_time:
-                    logging.info(f"Result: " + result)
+                with self.lock:
+                    if result not in last_detection_time:
+                        logging.info(f"Result: " + result)
 
-                    rect_color = (0, 255, 0)  # Green color
-                    rect_thickness = 2
-                    bbox = face.bbox.astype(int)
-                    cv2.rectangle(
-                        frame,
-                        (bbox[0], bbox[1]),
-                        (bbox[2], bbox[3]),
-                        rect_color,
-                        rect_thickness,
-                    )
-                    last_detection_time[result] = current_time
-                    timestamp = time.strftime("%Y/%m/%d")
-                    path = os.path.join(
-                        "media/screenshots/criminals/", result, str(timestamp)
-                    )
-                    filename = self.save_screenshot(frame, path)
-                    logging.info(f"Path of image: {path}")
-                    logging.info(f"file name: {filename}")
-                    logging.info(f"Adding to database: {result}")
-                    self.database.insert_records(
-                        image=f"http://{os.getenv('BASE_HOST')}:8000/"
-                        + path
-                        + "/"
-                        + filename,
-                        criminal=result,
-                        camera=self.database.get_camera(camera_index).get("id"),
-                        date_recorded=datetime.now(),
-                    )
+                        rect_color = (0, 255, 0)  # Green color
+                        rect_thickness = 2
+                        bbox = face.bbox.astype(int)
+                        cv2.rectangle(
+                            frame,
+                            (bbox[0], bbox[1]),
+                            (bbox[2], bbox[3]),
+                            rect_color,
+                            rect_thickness,
+                        )
+                        last_detection_time[result] = current_time
+                        timestamp = time.strftime("%Y/%m/%d")
+                        path = os.path.join(
+                            "media/screenshots/criminals/", result, str(timestamp)
+                        )
+                        filename = self.save_screenshot(frame, path)
+                        logging.info(f"Path of image: {path}")
+                        logging.info(f"file name: {filename}")
+                        logging.info(f"Adding to database: {result}")
+                        self.database.insert_records(
+                            image=f"http://{os.getenv('BASE_HOST')}:8000/"
+                            + path
+                            + "/"
+                            + filename,
+                            criminal=result,
+                            camera=self.database.get_camera(camera_index).get("id"),
+                            date_recorded=datetime.now(),
+                        )
 
     def save_screenshot(self, frame, save_path):
         if not os.path.exists(save_path):
@@ -157,7 +161,7 @@ class Surveillance:
 
     def reconnect_cameras(self):
         pending_cameras = []
-        while True:
+        while not self.exit_event.is_set():
             current_camera_urls = self.database.get_camera_urls()
             self.process_camera_frames_parallel(pending_cameras)
             pending_cameras.clear()
@@ -190,7 +194,7 @@ class Surveillance:
                 logging.info("Connected to camera: " + url)
 
             current_time = time.time()
-            while True:
+            while not self.exit_event.is_set():
                 ret, frame = camera.read()
                 if not ret:
                     logging.error(f"Error: Could not read frame from camera {url}")
@@ -202,7 +206,6 @@ class Surveillance:
                     "last_detection_time": self.last_detection_time,
                 }
                 self.process_frame(data)
-
                 if (time.time() - current_time) > 5:
                     save_screenshot(
                         frame=frame,
@@ -221,13 +224,32 @@ if __name__ == "__main__":
     root_directory = os.path.join(path, "media/criminals/")
     database = Database()
     face_trainer = Surveillance(root_directory, database)
-    reload_face_encodings_thread = threading.Thread(
-        target=face_trainer.reload_face_encodings
-    )
-    reload_face_encodings_thread.start()
-    reconnect_cameras_thread = threading.Thread(target=face_trainer.reconnect_cameras)
-    reconnect_cameras_thread.start()
-    threading.Thread(
-        target=face_trainer.process_camera_frames_parallel,
-        args=(database.get_camera_urls(),),
-    ).start()
+    try:
+        reload_face_encodings_thread = threading.Thread(
+            target=face_trainer.reload_face_encodings
+        )
+        reconnect_cameras_thread = threading.Thread(
+            target=face_trainer.reconnect_cameras
+        )
+
+        reload_face_encodings_thread.start()
+        reconnect_cameras_thread.start()
+
+        # Main thread
+        main_thread = threading.Thread(
+            target=face_trainer.process_camera_frames_parallel,
+            args=(database.get_camera_urls(),),
+        )
+        main_thread.start()
+
+        # Wait for all threads to finish
+        main_thread.join()
+        reload_face_encodings_thread.join()
+        reconnect_cameras_thread.join()
+
+    except KeyboardInterrupt:
+        # Handle KeyboardInterrupt (Ctrl+C) to exit the threads gracefully
+        face_trainer.exit_event.set()
+        main_thread.join()
+        reload_face_encodings_thread.join()
+        reconnect_cameras_thread.join()
